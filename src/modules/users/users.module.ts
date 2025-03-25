9080e8aa66f16e9432a73876ddf7588dbf98a379
ghostoflord <ghost2892003zzz@gmail.com>
2025-03-25 15:59:28 +0700

    update

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -5,9 +5,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
 
 @Module({
-  
+
   imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
   controllers: [UsersController],
   providers: [UsersService],
+  exports: [UsersService]
 })
 export class UsersModule { }
