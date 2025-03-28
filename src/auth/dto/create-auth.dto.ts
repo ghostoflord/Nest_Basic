017ce739f5deaf3970a2c03e57ad971cd84e6d7a
ghostoflord <ghost2892003zzz@gmail.com>
2025-03-28 15:30:21 +0700

    update 18

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -21,3 +21,18 @@ export class CodeAuthDto {
 
 }
 
+export class ChangePasswordAuthDto {
+    @IsNotEmpty({ message: "code không được để trống" })
+    code: string;
+
+    @IsNotEmpty({ message: "password không được để trống" })
+    password: string;
+
+    @IsNotEmpty({ message: "confirmPassword không được để trống" })
+    confirmPassword: string;
+
+    @IsNotEmpty({ message: "email không được để trống" })
+    email: string;
+
+}
+
